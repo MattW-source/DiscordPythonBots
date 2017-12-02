ed24c2fb9e66003e02561d83ce2d50ee8bec3483
FoggyIO <foggydog3799@gmail.com>
2017-12-02 17:19:52 +0000

    Create Tutorial 3.py

---
+++ Tutorial 3.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "!")
+
+
+@client.event
+async def on_ready():
+    print("Bot is online and connected to Discord")
+
+
+@client.event
+async def on_message(message):
+    if message.content.upper().startswith('!PING'):
+        userID = message.author.id
+        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
+    if message.content.upper().startswith('!SAY'):
+        if message.author.id == "<user id>": #Replace <User ID> with the ID of the user you want to be able to execute this command!
+            args = message.content.split(" ")
+            await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
+        else:
+            await client.send_message(message.channel, "You do not have permission")
+    if message.content.upper().startswith('!AMIADMIN'):
+        if "<role id>" in [role.id for role in message.author.roles]: #Replace <Role ID> with the ID of the role you want to be able to execute this command
+            await client.send_message(message.channel, "You are an admin")
+        else:
+            await client.send_message(message.channel, "You are not an admin")
+        
+
+
+client.run("<token>")
